9fce64a69dcb501f8102c11db03ae966e1469bdc
zb425 <1047441153@qq.com>
2020-09-20 22:22:13 +0800

    再次提交

--- locks/src/com/bgamq/locks/TestDemo.java
+++ locks/src/com/bgamq/locks/TestDemo.java
@@ -3,7 +3,8 @@ package com.bgamq.locks;
 public class TestDemo {
     public static void main(String[] args) {
 
-
         System.out.println("test");
+
+        System.out.println("我们是好的朋友");
     }
 }
